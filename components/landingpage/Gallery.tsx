9c03512f52cba790b4c01088c25aabfe5b2a3a66
AYENI DANIEL <starlordflash@gmail.com>
2023-10-31 20:55:56 +0100

    pushed now

--- components/landingpage/Gallery.tsx
+++ components/landingpage/Gallery.tsx
@@ -58,10 +58,10 @@ export default function Gallery() {
       <div className="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
         <div className="space-y-12">
           <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
-            <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">
+            <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl text-center">
               Explore Our Projects
             </h2>
-            <p className="text-xl text-gray-500">
+            <p className="text-base text-gray-500 text-center mx-auto">
               Each image tells a story of our dedication to excellence in the
               construction and agriculture sectors.
             </p>
