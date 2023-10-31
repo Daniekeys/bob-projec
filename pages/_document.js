d23d2f01344f85ba0612b8497f9b2e355e514151
AYENI DANIEL <starlordflash@gmail.com>
2023-10-31 21:46:47 +0100

    pushed

--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className='w-full'>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="ivekZK2st5P-eSqD-xPIS_nrjQ_IOUnXmYBraer2Tyw"
+        />
+      </Head>
+      <body className="w-full">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
